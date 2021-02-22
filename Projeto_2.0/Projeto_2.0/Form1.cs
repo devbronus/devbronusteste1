38578b3c5e4a2b54d0515107be9f36fe4daa013e
Bruno Reis <68503895+devbronus@users.noreply.github.com>
2021-02-22 14:05:02 -0300

    Calculadora em Windows Forms C#

---
+++ Projeto_2.0/Projeto_2.0/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int valor1, valor2, resultado;
+
+            valor1 = Convert.ToInt32(textBox1.Text);
+            valor2 = Convert.ToInt32(textBox2.Text);
+
+            resultado = valor1 + valor2;
+
+            richTextBox1.Text = "A soma dos número é: \r" + resultado.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int valor1, valor2, resultado;
+
+            valor1 = Convert.ToInt32(textBox1.Text);
+            valor2 = Convert.ToInt32(textBox2.Text);
+
+            resultado = valor1 - valor2;
+
+            richTextBox1.Text = "A subtração dos números é: \r" + resultado.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int valor1, valor2, resultado;
+
+            valor1 = Convert.ToInt32(textBox1.Text);
+            valor2 = Convert.ToInt32(textBox2.Text);
+
+            resultado = valor1 * valor2;
+
+            richTextBox1.Text = "A multiplicação dos números é: \r" + resultado.ToString();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int valor1, valor2, resultado;
+
+            valor1 = Convert.ToInt32(textBox1.Text);
+            valor2 = Convert.ToInt32(textBox1.Text);
+
+            resultado = valor1 / valor2;
+
+            richTextBox1.Text = "A divisão dos número é: \r" + resultado.ToString();
+
+        }
+    }
+}
